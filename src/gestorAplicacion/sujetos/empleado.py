65bd5e3daf121a4659a0cd7e7d98dbef5d694f0a
juandiegoordonezl <159456074+juandiegoordonezl@users.noreply.github.com>
2024-08-31 18:31:01 -0500

    Traducción parte sujetos

---
+++ src/gestorAplicacion/sujetos/empleado.py
@@ -0,0 +1,64 @@
+from gestorAplicacion.sujetos.persona import Persona
+from gestorAplicacion.servicios.enums import Genero, TipoEmpleado
+from gestorAplicacion.servicios.tienda import Tienda
+
+class Empleado(Persona):
+    # Atributos----------------------------------------------------------------------------------------------------
+    
+    def __init__(self, nombre=None, id=None, edad=None, genero=None, liquidacion=0.0, tienda=None,prestacionSalud=False, prestacionPension=False, tipo=None, experiencia=0):
+        super().__init__(nombre, id, edad, genero)
+        self.liquidacion = liquidacion
+        self.prestacionSalud = prestacionSalud
+        self.prestacionPension = prestacionPension
+        self.experiencia = experiencia
+        self.tienda = tienda
+        self.tipo = tipo
+
+    # Métodos------------------------------------------------------------------------------------------------------
+    
+    def get_tipo(self):
+        return self.tipo
+
+    def get_liquidacion(self):
+        return self.liquidacion
+
+    def set_liquidacion(self, liquidacion):
+        self.liquidacion = liquidacion
+
+    def get_tienda(self):
+        return self.tienda
+
+    def set_tienda(self, tienda):
+        self.tienda = tienda
+
+    def is_prestacion_salud(self):
+        return self.prestacionSalud
+
+    def set_prestacion_salud(self, prestacion_salud):
+        self.prestacionSalud = prestacion_salud
+
+    def is_prestacion_pension(self):
+        return self.prestacionPension
+
+    def set_prestacion_pension(self, prestacion_pension):
+        self.prestacionPension = prestacion_pension
+
+    def get_experiencia(self):
+        return self.experiencia
+
+    def set_experiencia(self, experiencia):
+        self.experiencia = experiencia
+
+    def set_tipo(self, tipo):
+        self.tipo = tipo
+
+    # Métodos abstractos------------------------------------------------------------------------------------------------------
+
+    def busco_chamba(self):
+        raise NotImplementedError("Este método debe ser implementado por la subclase")
+
+    def cantidad_pago(self):
+        raise NotImplementedError("Este método debe ser implementado por la subclase")
+
+    def validar_criterios(self):
+        raise NotImplementedError("Este método debe ser implementado por la subclase")
