ef6512667bb76ab040dcbcb17b67ab3aa41469b0
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-09-15 10:29:57 -0500

    Ignorar los pyc cache, crear hojaVida.py y beinvenida.py
    * hojaVida.py
    
    * from uiMain.

---
+++ src/uiMain/bienvenida.py
@@ -0,0 +1,61 @@
+
+import os
+import pathlib
+from tkinter import BOTH, INSERT, X, Button, Frame, Label, PhotoImage, scrolledtext
+
+from uiMain.interfaz import Iniciar_ventana_usuario
+
+
+class Bienvenida(Frame):
+    def __init__(self, window):
+        super().__init__(window)
+        self._window = window
+
+        # Divisiones P3 y P4 en P1
+        self.frame_p3 = Frame(self._window, height=180, padx=5, pady=5, bd=2, relief="solid",bg="light blue")
+        self.frame_p3.pack(fill=X, padx=5, pady=5)
+
+        self.frame_p4 = Frame(self._window, height=500, padx=5, pady=5, bd=2, relief="solid",bg="light blue")
+        self.frame_p4.pack(fill=BOTH, expand=True, padx=5, pady=(5,0), anchor='s')
+
+        self._next_el = 0
+
+        saludo = Label(self.frame_p3, text="Bienvenido a My_Tiendita donde podrás realizar tus compras o administrar tus tiendas", font=("Helvetica", 10, "bold"),bg="light blue")
+        saludo.pack(pady=5)
+
+        self.frame_aux =Frame(self.frame_p3, height=120,bg="light blue")
+        self.frame_aux.pack(fill=BOTH, expand=True)
+
+        # self.saludo2.pack(fill=BOTH, expand=True, pady=5)
+        self.saludo2 = scrolledtext.ScrolledText(self.frame_aux, height=8, width=77, padx=5)
+        self.saludo2.tag_configure("center", justify="center")
+        self.saludo2.insert(INSERT, "Descripcion texto largo de software My_Tiendita")
+
+
+        self._pantallazos = []
+        for i in range(0,5):
+            path = os.path.join(pathlib.Path(__file__).parent.parent.parent.absolute(), 'src\\assets\\pantallazos\\pantallazo'+str(i+1)+'.png')
+            pantallazo = PhotoImage(file=path)
+            self._pantallazos.append(pantallazo)
+
+        self._label = Label(self.frame_p4, image=self._pantallazos[0], height=350, width=600)
+        self._label.bind('<Leave>', self.proximo)
+        self._label.pack(pady=5,fill="both", expand=True)
+
+        button = Button(self.frame_p4, text="Aca se ira a la otra ventana", command=self.abrir_ventana_principal)
+        button.pack(side="bottom", pady=5)
+
+    # Actualiza el proximo pantallazo de la aplicacion
+    def proximo(self, _):
+        if self._next_el < 4:
+            self._next_el = self._next_el + 1
+        else:
+            self._next_el = 0
+
+        self._label.configure(image=self._pantallazos[self._next_el])
+        self._label.image = self._pantallazos[self._next_el]
+
+    # Carga la ventana principal del admin y cierra la ventana actual
+    def abrir_ventana_principal(self):
+        self._window.master.master.destroy()
+        Iniciar_ventana_usuario()
---
+++ src/uiMain/hojaVida.py
@@ -0,0 +1,83 @@
+import os
+import pathlib
+from tkinter import BOTH, INSERT, X, Frame, Label, PhotoImage, Text
+
+
+class HojaVida(Frame):
+    _posicion_imagen = [(0, 0), (0, 1), (1, 0), (1, 1)]
+
+    def __init__(self, window):
+        super().__init__(window)
+
+        self.frame_p5 = Frame(window, height=180, padx=5, pady=5, bd=2, relief="solid",bg="light blue")
+        self.frame_p5.pack(fill=X, padx=5, pady=5)
+
+        self.frame_p6 = Frame(window, padx=5, pady=5, bd=2, relief="solid",bg="light blue")
+        self.frame_p6.pack(fill=BOTH, expand=True, padx=5, pady=5)
+
+        self._text = None
+        self._next_hv = 0
+        self._photos = [None, None, None, None]
+        self._labels = []
+        self.cargar_hv(0)
+
+        for i in range(0, 4):
+            label = Label(self.frame_p6)
+            (r, c) = HojaVida._posicion_imagen[i]
+            label.grid(row=r, column=c, padx=5, pady=5, sticky="nsew")
+            self.frame_p6.grid_rowconfigure(r, weight=1)
+            self.frame_p6.grid_columnconfigure(c, weight=1)
+            self._labels.append(label)
+            self.cargar_hv_imagen(0, i)
+        self.pack()
+
+    def cargar_hv_imagen(self, hv_num, numero):
+        if self._next_hv==0:
+            png="jhorman/foto"+str(numero+1)
+        elif self._next_hv==1:
+            png="juanes/foto"+str(numero+1)
+        elif self._next_hv == 2:
+            png = "juanDiego/foto"+str(numero+1)
+        else:
+            png="jordan/foto"+str(numero+1)
+        path = os.path.join(pathlib.Path(__file__).parent.parent.parent.absolute(), 'src', 'assets', png+'.png')
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Image file not found at path: {path}")
+
+        # Redimensionar la imagen usando subsample
+        photo = PhotoImage(file=path)
+        photo = photo.subsample(2, 2)  # Ajusta los valores según sea necesario
+
+        self._photos[numero] = photo  # Mantén la referencia a la imagen
+        self._labels[numero].configure(image=photo)
+        self._labels[numero].image = photo
+
+    def cargar_hv(self, numero):
+        self._text = Text(self.frame_p5, height=10, width=77)
+        self._text.pack(fill=BOTH, expand=True)
+        self._text.bind('<Button>', self.proximo)
+
+        #path = os.path.join(pathlib.Path(__file__).parent.parent.parent.absolute(), 'src\\assets\\jhorman.txt'.format(numero))
+        if self._next_hv==0:
+            txt="jhorman"
+        elif self._next_hv==1:
+            txt="juanes"
+        elif self._next_hv == 2:
+            txt = "juanDiego"
+        else:
+            txt="jordan"
+
+        with open(os.path.join(pathlib.Path(__file__).parent.parent.parent.absolute(), 'src\\assets\\'+txt+".txt"), "r+") as hv_text:
+            self._text.insert(INSERT, hv_text.read())
+
+    def proximo(self, _):
+        self._text.destroy()
+        if self._next_hv < 3:
+            self._next_hv = self._next_hv + 1
+        else:
+            self._next_hv = 0
+
+        self._photos = [None, None, None, None]
+        self.cargar_hv(self._next_hv)
+        for i in range(0, 4):
+            self.cargar_hv_imagen(self._next_hv, i)
