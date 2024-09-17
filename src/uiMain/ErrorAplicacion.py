0b3680b1ef3528d914a2ce9c047f6c776ebc3f41
Juan Diego ORdoñe <juordonezl@unal.edu.co>
2024-09-17 01:19:39 -0500

    excepción

--- src/uiMain/ErrorAplicacion.py
+++ src/uiMain/ErrorAplicacion.py
@@ -13,8 +13,15 @@ class CategoriaPropia2(ErrorAplicacion):
     def __init__(self,mensaje):
         super().__init__(mensaje)
 
-class ExceptionInventada1(CategoriaPropia1):
-    pass
+class TiendaNoAsignadaError(Exception):
+    def __init__(self, message="Debe pasar por la funcionalidad 1 para asignar una tienda."):
+        self.message = message
+        super().__init__(self.message)
+
+# Función que verifica si el cliente tiene una tienda asignada
+def verificar_tienda_asignada(cliente):
+    if cliente.get_tienda() is None:  
+        raise TiendaNoAsignadaError()
 
 class ExceptionInventada2(CategoriaPropia1):
     pass
